776f20b5c0c1767d64dce00f36123ffb82980158
Jingru Xu <nicoxu1000@gmail.com>
2023-08-12 15:33:12 -0400

    Add the first version that works

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const computerChoices = ["rock", "paper", "scissor"];
 
-let computerSelect = computerChoices[Math.floor(Math.random() * 3)];
+let computerSelect;
+//  = computerChoices[Math.floor(Math.random() * 3)];
+let playerSelect;
+// = prompt("paper, scissor or rock ? Pick One").toLowerCase();
 
-let playerSelect = prompt("paper, scissor or rock ? Pick One").toLowerCase();
-
-function getGameResult(playerSelection, computerSelection) {
+let computerScore = 0;
+let playerScore = 0;
+function playSingleRound(playerSelection, computerSelection) {
   if (
     playerSelection != "paper" &&
     playerSelection != "rock" &&
@@ -14,21 +17,44 @@ function getGameResult(playerSelection, computerSelection) {
   } else if (computerSelection == playerSelection) {
     alert("U tied!");
   } else if (computerSelection == "rock" && playerSelection == "paper") {
+    playerScore++;
     alert("U win!");
   } else if (computerSelection == "rock" && playerSelection == "scissor") {
+    computerScore++;
     alert("U lose!");
   } else if (computerSelection == "paper" && playerSelection == "scissor") {
+    playerScore++;
     alert("u Win!");
   } else if (computerSelection == "paper" && playerSelection == "rock") {
+    computerScore++;
     alert("U lose!");
   } else if (computerSelection == "scissor" && playerSelection == "paper") {
+    computerScore++;
     alert("U lose !");
   } else if (computerSelection == "scissor" && playerSelection == "rock") {
+    playerScore++;
     alert("U win!");
   }
+
+  return [playerScore, computerScore];
 }
 
-console.log(playerSelect);
-console.log(computerSelect);
+// game function to run the previous playSingleRound 5 times, and return the computer score and player score after 5 times, and based on the result, determine the final winner.
 
-console.log(getGameResult(playerSelect, computerSelect));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    computerSelect = computerChoices[Math.floor(Math.random() * 3)];
+    playerSelect = prompt("paper, scissor or rock ? Pick One").toLowerCase();
+    playSingleRound(playerSelect, computerSelect);
+  }
+
+  const Result = [playerScore, computerScore];
+  console.log(Result);
+
+  if (Result[0] > Result[1]) {
+    alert("u win! yeah");
+  } else if (Result[0] < Result[1]) alert("U lose, no!");
+  alert("tied!!!");
+}
+
+game();
