df587151c9ea2f78b665215f3b1012eafe4533fc
Jingru Xu <nicoxu1000@gmail.com>
2023-11-13 23:50:17 -0500

    Add win/lose scenario and audio affectfor click

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const computerChoices = ["rock", "paper", "scissor"];
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
 let computerScore;
 let playerScore;
 
 function game(playerSelection, computerSelection) {
-  console.log(playerSelection, computerSelection);
-
   let showPlayerSelection = document.createElement("p");
   showPlayerSelection.textContent = `You chose ${playerSelection}`;
 
@@ -21,6 +20,30 @@ function game(playerSelection, computerSelection) {
       document.createElement("h2")
     ).textContent = "It's a tie";
   }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissor" && computerSelection === "paper")
+  ) {
+    showComputerSelection.insertAdjacentElement(
+      "afterend",
+      document.createElement("h2")
+    ).textContent = "You won";
+    playerScore++;
+  }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "rock")
+  ) {
+    showComputerSelection.insertAdjacentElement(
+      "afterend",
+      document.createElement("h2")
+    ).textContent = "You lost";
+    computerScore++;
+  }
 }
 
 const display = document.querySelector(".display");
@@ -34,11 +57,16 @@ function addInstructions() {
   const h2 = document.createElement("h2");
   h2.textContent = "Pick one above";
   instructionsArea.appendChild(h2);
+  h2.style.textAlign = "center";
+
   display.insertAdjacentElement("afterend", instructionsArea);
   btn.removeEventListener("click", addInstructions);
 
   choices.forEach((choice) => {
     choice.addEventListener("click", (e) => {
+      choice.addEventListener("click", () => {
+        audio.play();
+      });
       h2.remove();
       playerSelect = e.currentTarget.id;
       computerSelect = computerChoices[Math.floor(Math.random() * 3)];
